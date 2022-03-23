98ddb5ac38253677e6dde889bba46dbf85325d37
şahan boz <bozsahan@gmail.com>
2022-03-23 18:46:09 +0300

    Create directory model

---
+++ Model/Directory.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Directory.Model
+{
+    public class Directory
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int UUID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Company { get; set; }
+    }
+}
