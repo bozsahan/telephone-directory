7bd71c9ddad927d026a668d550d02c99dcf0322e
şahan boz <bozsahan@gmail.com>
2022-03-23 18:48:05 +0300

    refactor model folder

--- Models/Directory.cs
+++ Models/Directory.cs
@@ -1,7 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Directory.Model
+namespace Directory.Models
 {
     public class Directory
     {
