3eabfa3d736aaab4f5c2c8b527a3219ce809af39
şahan boz <bozsahan@gmail.com>
2022-03-23 19:48:25 +0300

    Create Directory Controller

---
+++ Controllers/DirectoryContoller.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Directory.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DirectoryContoller : ControllerBase
+    {
+    }
+}
