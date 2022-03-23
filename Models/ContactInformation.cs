7fe5f664afd24cb9b0f5233883cbee2b8543eaa8
şahan boz <bozsahan@gmail.com>
2022-03-24 00:42:00 +0300

    Create ContactInformation Model

---
+++ Models/ContactInformation.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Directory.Models
+{
+    public class ContactInformation
+    {    
+        public string PhoneNumber { get; set; }
+        public string Email{ get; set; }
+        public string Location { get; set; }
+
+        public Collection<TelephoneDirectory> TelephoneDirectory { get; set; }
+        
+    }
+}
--- Models/TelephoneDirectory.cs
+++ Models/TelephoneDirectory.cs
@@ -1,11 +1,17 @@
-﻿namespace Directory.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Directory.Models
 {
     public class TelephoneDirectory
     {
-        
+        [Key]
         public int UUID { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
         public string Company { get; set; }
+
+        [ForeignKey("TID")]
+        public ContactInformation ContactInformation { get; set; }
     }
 }
